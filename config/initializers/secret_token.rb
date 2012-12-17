591836498e022f06a1c9a1518857f9b189e42951
Aldehir Rojas <aldehir@visualcoding.net>
2012-12-17 16:41:23 -0600

    Initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dirtnapgaming::Application.config.secret_token = '50531ad59c60c9439de3fc618e92e0adb02056c918fb997e2d35e4ba89489b998371bfc7416dfaa8838e4a1a71eeaefe40225daf4adb772e6b95553eb8703957'
